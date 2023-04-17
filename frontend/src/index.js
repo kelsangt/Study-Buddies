35ce3d92736d856962017b4afde2669a1ddaf4ae
Kelly Zhou <yinglinzhou0@gmail.com>
2023-04-17 11:46:01 -0400

    eventReducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,14 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store/store';
+import * as eventActions from './store/events';
 
 let store = configureStore({});
 
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.eventActions = eventActions;
+}
 function Root() {
   return (
     <Provider store={store}>
