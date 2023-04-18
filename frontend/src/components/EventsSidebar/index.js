54e6a3c3664bcdde4713e9904a3f83fdc7e8db73
Fahim Khan <fk652@nyu.edu>
2023-04-18 15:01:38 -0400

    update scrolling and event listing

--- frontend/src/components/EventsSidebar/index.js
+++ frontend/src/components/EventsSidebar/index.js
@@ -1,9 +1,36 @@
+import { useSelector } from 'react-redux';
+import './EventSidebar.css';
+import { getEvents } from '../../store/events';
+import EventSidebarItem from './EventSidebarItem';
 
 const EventSideBar = () => {
+  const events = useSelector(getEvents);
 
   return (
     <div className="event-sidebar">
-      
+      {
+        events.map(event => {
+          return <EventSidebarItem event={event} key={event._id}/>
+        })
+      }
+
+{
+        events.map(event => {
+          return <EventSidebarItem event={event} key={event._id}/>
+        })
+      }
+
+{
+        events.map(event => {
+          return <EventSidebarItem event={event} key={event._id}/>
+        })
+      }
+
+{
+        events.map(event => {
+          return <EventSidebarItem event={event} key={event._id}/>
+        })
+      }
     </div>
   )
 }
