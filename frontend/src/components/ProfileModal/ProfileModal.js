44e72010b5dd40a20020999cbd4713d309659165
Kelly Zhou <yinglinzhou0@gmail.com>
2023-04-19 11:26:23 -0400

    added tabs to profile slideout

--- frontend/src/components/ProfileModal/ProfileModal.js
+++ frontend/src/components/ProfileModal/ProfileModal.js
@@ -4,33 +4,72 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { getMyCreatedEvents, getMyJoinedEvents } from '../../store/events';
 import MyCreatedEvents from '../MyCreatedEvents/MyCreatedEvents';
+import { useState } from 'react';
+import { selectedTab, setTabStatus } from '../../store/ui';
 const ProfileModal = () => {
     const dispatch = useDispatch();
     const createdEvents = useSelector(getMyCreatedEvents);
     const joinedEvents = useSelector(getMyJoinedEvents);
+    const requestedEvents = useSelector(state => state.session.user.requestedEvents);
+
+    const currentTab = useSelector(selectedTab);
+    let myEventTab;
+    let joinedEventTab;
+    let requestedEventTab;
+    if (currentTab === 'My Events') {
+        myEventTab = true
+    } else if (currentTab === 'Joined Events') {
+        joinedEventTab = true
+    } else {
+        requestedEventTab = true
+    }
 
     const logoutUser = e => {
         e.preventDefault();
         dispatch(logout());
-      }
+    }
+    const selectMyEvents = () => {
+        dispatch(setTabStatus("My Events"))
+    }
+
+    const selectJoinedEvents = () => {
+        dispatch(setTabStatus("Joined Events"))
+    }
+    const selectRequestEvents = () => {
+        dispatch(setTabStatus("Requested Events"))
+    }
+
     return (
         <div id='profile-modal-container'> 
             <div id='big-event-container'>
                 <div id='myevents-create-container'>
                     <div id='events-tab'>
-                        <div className='myevents-header'>My Events</div>
-                        <div className='myevents-header'>Joined Events</div>
-                        <div className='myevents-header' id='myevents-header-last'>Requested Events</div>
+                        <div className={`myevents-header ${currentTab === "My Events" ? 'selected' : ''}`} onClick={selectMyEvents}>My Events</div>
+                        <div className={`myevents-header ${currentTab === "Joined Events" ? 'selected' : ''}`} onClick={selectJoinedEvents}>Joined Events</div>
+                        <div className={`myevents-header ${currentTab === "Requested Events" ? 'selected' : ''}`} onClick={selectRequestEvents}>Requested Events</div>
                     </div>
                     <div id='create-event-button'>
                         <i className="fa-solid fa-plus" id='plus-icon'></i>
                         Create Event</div>
-                    <div id='divider'></div>
                 </div>
 
                 <div id='events-holder'>
                     {
+                        myEventTab &&
                         createdEvents.map(event => {
+                            return <MyCreatedEvents event={event} key={event._id}/>
+                        })
+                    }
+                    {
+                        joinedEventTab &&
+                        joinedEvents.map(event => {
+                            return <MyCreatedEvents event={event} key={event._id}/>
+                        })
+                    }
+
+                    {
+                        requestedEventTab &&
+                        requestedEvents.map(event => {
                             return <MyCreatedEvents event={event} key={event._id}/>
                         })
                     }
