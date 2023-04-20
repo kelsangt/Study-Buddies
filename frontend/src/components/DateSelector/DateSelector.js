44d77fc9e9a81b0bb7025352152102a00adb9cd2
Kelly Zhou <yinglinzhou0@gmail.com>
2023-04-20 11:24:30 -0400

    date selector done

--- frontend/src/components/DateSelector/DateSelector.js
+++ frontend/src/components/DateSelector/DateSelector.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import DatePicker from "react-datepicker";
 import "./DateSelector.css";
 import { useDispatch } from 'react-redux';
+import { fetchAllEventsForDay } from '../../store/events';
 
 
 const DateSelector = () => {
@@ -9,11 +10,12 @@ const DateSelector = () => {
     const dispatch = useDispatch();
 
     const handleSelectedDate = () => {
-        
+        dispatch(fetchAllEventsForDay(chosenDate.toISOString().split("T")[0]))
     }
 
     return (
         <DatePicker 
+        showIcon
         selected={chosenDate} 
         onChange={(date) => setChosenDate(date)}
         onSelect={handleSelectedDate}
