030568344dc230ba8b89099f98cb46e25ebeb815
Kelsang <kelsangrtsering@gmail.com>
2023-04-20 06:14:46 -0400

    get event update modal to work

---
+++ frontend/src/components/EventUpdateForm/EventUpdateForm.js
@@ -0,0 +1,8 @@
+const EventUpdateForm = () => {
+    return (
+        <h1>HELLO</h1>
+    )
+}
+
+
+export default EventUpdateForm;
--- frontend/src/components/MyCreatedEvents/MyCreatedEvents.js
+++ frontend/src/components/MyCreatedEvents/MyCreatedEvents.js
@@ -1,16 +1,46 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { NameToolTip } from '../../context/Modal';
 import './MyCreatedEvents.css';
 import { selectedTab } from '../../store/ui';
+import { CenterModal } from '../../context/Modal';
+import { setModalStatus } from '../../store/ui';
+import EventUpdateForm from '../EventUpdateForm/EventUpdateForm';
 
 const MyCreatedEvents = ({event}) => {
     const [showModal, setShowModal] = useState(false);
     const [top, setTop] = useState(0);
     const [left, setLeft] = useState(0);
     const [currentModal, setCurrentModal] = useState(null);
+    const [showEventEditModal, setShowEventEditModal] = useState(false);
     const currentTab = useSelector(selectedTab);
     const count = event.attendees.length
+    const dispatch = useDispatch();
+
+    const showEditForm = (e) => {
+        e.preventDefault();
+        setShowEventEditModal(true);
+        const sideModal = document.getElementById('profile-modal-container');
+        sideModal.style.display = 'flex';
+    }
+
+    const hideEditForm = (e) => {
+        e.preventDefault();
+        setShowEventEditModal(false);
+        dispatch(setModalStatus(true));
+        const sideModal = document.getElementById('profile-modal-container');
+        sideModal.style.display = 'flex';
+    }
+
+
+    const closeModal2 = () => {
+        const modal = document.getElementById('profile-modal-container')
+        modal.classList.add('slideout')
+        console.log(modal)
+        setTimeout(()=>{
+        dispatch(setModalStatus(false))
+      }, 500)
+    }
 
     return (
         <div className='individual-event-holder'>
@@ -31,7 +61,12 @@ const MyCreatedEvents = ({event}) => {
             </div>
             {(currentTab === "My Events") &&
             <div className='myevent-edit-holder'>
-                <div id='myevent-edit-button'>Edit Event</div>
+                <div onClick={showEditForm} id='myevent-edit-button'>Edit Event</div>
+                {showEventEditModal && (
+                    <CenterModal onClose={hideEditForm}>
+                        <EventUpdateForm />
+                    </CenterModal>
+                )}
             </div>
             }
         </div>
