cabcdd221e8a049de6cdf9f1a28f2f558a85a0bd
Kelsang <kelsangrtsering@gmail.com>
2023-04-20 22:44:18 -0400

    work on styling various components

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -3,7 +3,7 @@ import NavBar from "../NavBar/NavBar";
 import './SplashPage.css';
 import TeamList from "./TeamList";
 import splashimage1 from './assets/splashimage1.png';
-import splashimage3 from './assets/splashimage3.png';
+import splashmapimage from './assets/splashmapimage.png';
 import Footer from "../Footer/Footer";
 import { useEffect, useState } from 'react';
 import { AdToolTip } from '../../context/Modal';
@@ -130,9 +130,13 @@ function SplashPage() {
             </div>
             <div>
             <div id="splashDiv2">
-              <img src={splashimage3} alt="splashimage3" id="splashimage3"/> 
+              <img src={splashmapimage} alt="splashmapimage" id="splashmapimage"/> 
               <div id="tagline2">
-                Attend study sessions in libraries across the United States
+                <h1 id="tagline2H1">Attend study sessions in libraries across the United States</h1>
+                <div id="taglineInnerDiv">
+                  <h3 id="initialH3">Meet people studying topics you are interested in, grow your network, learn.</h3>
+                  <h3>With easy to set up study sessions, you will always be able to contact students interested the same field of study as you.</h3>
+                </div>
               </div>
               <p></p>
             </div>
