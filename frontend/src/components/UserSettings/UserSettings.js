89c1f8d3dab6c147bef4c2fc51c38bd4b2b6e77d
Kelly Zhou <yinglinzhou0@gmail.com>
2023-04-25 16:07:21 -0400

    user settings edit

--- frontend/src/components/UserSettings/UserSettings.js
+++ frontend/src/components/UserSettings/UserSettings.js
@@ -1,4 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { clearSessionErrors } from '../../store/session';
 import { useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import './UserSettings.css'
@@ -19,8 +21,49 @@ const UserSettings = () => {
     const [major, setMajor] = useState(user.major)
     const [linkedInUrl, setLinkedInUrl] = useState(user.linkedInUrl)
     const [phoneNum, setPhoneNum] = useState(user.phone)
+    const errors = useSelector(state => state.errors.session);
 
-
+    console.log(errors)
+    useEffect(() => {
+        return () => {
+          dispatch(clearSessionErrors());
+        };
+      }, [dispatch]);
+    
+    const update = field => {
+        let setState;
+    
+        switch (field) {
+            case 'firstName':
+                setState = setFirstName;
+                break;
+            case 'lastName':
+                setState = setLastName;
+                break;
+            case 'username':
+                setState = setUsername;
+                break;
+            case 'email':
+                setState = setEmail;
+                break;
+            case 'school':
+                setState = setSchool;
+                break;
+            case 'major':
+                setState = setMajor;
+                break;
+            case 'linkedInUrl':
+                setState = setLinkedInUrl;
+                break;
+            case 'phone':
+                setState = setPhoneNum;
+                break;
+            default:
+                throw Error('Unknown field in Edit Form');
+        }
+    
+        return e => setState(e.currentTarget.value);
+      }
 
     const editSettings = () => {
         let get = Array.from(document.getElementsByClassName('edit-profile-input'))
@@ -39,6 +82,7 @@ const UserSettings = () => {
         })
 
     }
+
 
     return (
         <>
@@ -63,14 +107,15 @@ const UserSettings = () => {
                 <div id='user-profile-edit-container'>
 
 
-                    <div id='display-profile-info-container'>
+                    <form id='display-profile-info-container' onSubmit={handleUpdate}>
                         <div>
                             <div className='usersettings-label'>First Name</div>
                             <input className='edit-profile-input'
                                     value={firstName}
-                                    onChange={(e) => setFirstName(e.target.value)}
+                                    onChange={update('firstName')}
                                     type='text'
                                     disabled
+                                    required
                                     />
                         </div>
 
@@ -78,9 +123,10 @@ const UserSettings = () => {
                             <div className='usersettings-label'>Last Name</div>
                             <input className='edit-profile-input'
                                     value={lastName}
-                                    onChange={(e) => setLastName(e.target.value)}
+                                    onChange={update('lastName')}
                                     type='text'
                                     disabled
+                                    required
                                     />
                         </div>
 
@@ -88,9 +134,10 @@ const UserSettings = () => {
                             <div className='usersettings-label'>Username</div>
                             <input className='edit-profile-input'
                                     value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
+                                    onChange={update('username')}
                                     type='text'
                                     disabled
+                                    required
                                     />
                         </div>
 
@@ -98,9 +145,10 @@ const UserSettings = () => {
                             <div className='usersettings-label'>Email</div>
                             <input className='edit-profile-input'
                                     value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
+                                    onChange={update('email')}
                                     type='email'
                                     disabled
+                                    required
                                     />
                         </div>
 
@@ -108,9 +156,10 @@ const UserSettings = () => {
                             <div className='usersettings-label'>School</div>
                             <input className='edit-profile-input'
                                     value={school}
-                                    onChange={(e) => setSchool(e.target.value)}
+                                    onChange={update('school')}
                                     type='text'
                                     disabled
+                                    required
                                     />
                         </div>
 
@@ -118,9 +167,10 @@ const UserSettings = () => {
                             <div className='usersettings-label'>Major</div>
                             <input className='edit-profile-input'
                                     value={major}
-                                    onChange={(e) => setMajor(e.target.value)}
+                                    onChange={update('major')}
                                     type='text'
                                     disabled
+                                    required
                                     />
                         </div>
 
@@ -128,11 +178,12 @@ const UserSettings = () => {
                             <div className='usersettings-label'>LinkedIn Url</div>
                             <input className='edit-profile-input'
                                     value={linkedInUrl}
-                                    onChange={(e) => setLinkedInUrl(e.target.value)}
+                                    onChange={update('linkedInUrl')}
                                     type='url'
                                     placeholder="https://www.linkedin.com/in/<EXAMPLE>"
                                     pattern="https://www.linkedin.com/in/*"
                                     disabled
+                                    required
                                     />
                         </div>
 
@@ -140,15 +191,24 @@ const UserSettings = () => {
                             <div className='usersettings-label'>Phone</div>
                             <input className='edit-profile-input'
                                     value={phoneNum}
-                                    onChange={(e) => setPhoneNum(e.target.value)}
+                                    onChange={update('phone')}
                                     type='tel'
                                     placeholder="123-45-678" 
-                                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                                     disabled
+                                    required
                                     />
                         </div>
-                    <div id='savesettings-button' onClick={handleUpdate}>Save Settings</div>
-                    </div>
+                        
+                        <div id='savesettings-button-holder'>
+                        <input className='savesettings-button' 
+                                type='submit'
+                                value="Save Settings"
+                                disabled={!email || !username || !firstName || !lastName || !school || !major}
+                                ></input>
+                        </div>
+
+                    </form>
 
                 </div>
             </div>
